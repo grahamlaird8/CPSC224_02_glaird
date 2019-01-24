2c0788f7d3570c10a149dc8c1f8252b0730d5172
Graham <grahamlaird8@gmail.com>
2019-01-24 12:47:12 -0800

    This is a test to get used to Github

---
+++ Jan24.java
@@ -0,0 +1,15 @@
+package jan24;
+
+
+public class Jan24 {
+
+    public static void main(String[] args) {
+        int a = 3;
+		int b = 5;
+		System.out.println(add(a, b));
+    }
+    
+	public static int add(int a, int b) {
+		return a + b;
+	}
+}
