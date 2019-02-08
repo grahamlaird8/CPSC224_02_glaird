556bae6dfc5aaf6f0068d05982cebbac00aebf42
FinnAllen <finnallen@protonmail.com>
2019-02-07 16:52:28 -0800

    began branch

--- HW2.java
+++ HW2.java
@@ -31,12 +31,12 @@ public class HW2 {
 	
 	public static String getWordFromUser()
 	{
-		
+		//Finn
 	}
 	
 	public static char getLetterFromUser()
 	{
-		
+		//Finn
 	}
 	
 	public static void printWordInProgress(String& inProgressWord, char guess, int sizeOfWord)
