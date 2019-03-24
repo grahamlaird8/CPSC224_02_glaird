81fdf43739e6ea74fd77d78456dbde1940fe6e66
Graham <grahamlaird8@gmail.com>
2019-03-24 14:17:09 -0700

    Update HW4.java
    Added the mouse and time listener outlines

--- HW4.java
+++ HW4.java
@@ -13,5 +13,50 @@ public class HW4 {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    private class MyMouseListener
+                         implements MouseListener
+   {
+      public void mousePressed(MouseEvent e)
+      {
+      }
+
+      public void mouseClicked(MouseEvent e)
+      {     
+      }
+
+      public void mouseReleased(MouseEvent e)
+      {
+      }
+
+      public void mouseEntered(MouseEvent e)
+      {
+      }
+
+      public void mouseExited(MouseEvent e)
+      {
+      }
+   }
+
+   /**
+      Private inner class to handle mouse motion events.
+   */
+   
+   private class MyMouseMotionListener
+                         implements MouseMotionListener
+   {
+      public void mouseDragged(MouseEvent e)
+      {
+      }
+
+      public void mouseMoved(MouseEvent e)
+      {    
+      }
+   }
+   
+   private class MyActionListener implements ActionListener
+   {
+       public void actionPerformed(ActionEvent e)
+       {
+       }     
+   }
 }
